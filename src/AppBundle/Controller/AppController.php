6dcf5204e24702d3c596efddff1fd0978fbf5cd0
Etienne Dauvergne <contact@ekyna.com>
2017-03-13 11:23:29 +0100

    Added public templates and controller.

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -14,12 +14,49 @@ class AppController extends Controller
     /**
      * App index action.
      *
-     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        $products = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        return $this->render('AppBundle:App:index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * App product detail action.
+     *
+     * @param int $productId
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function productAction($productId)
     {
-        return $this->render('AppBundle:App:index.html.twig');
+        $product = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->find($productId);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        $offers = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Offer')
+            ->findBy(
+                ['product' => $product],
+                ['price' => 'ASC']
+            );
+
+        return $this->render('AppBundle:App:product.html.twig', [
+            'product' => $product,
+            'offers'  => $offers,
+        ]);
     }
 }
